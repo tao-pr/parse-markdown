7743f1ebd5cd9f1d5e557b762cc9e19b64901f2d
starcolon <tao@starcolon.com>
2018-04-29 16:27:04 +0700

    using promise

---
+++ samples/primitive.md
@@ -0,0 +1,19 @@
+# Headline
+
+Some text under the headline with `inline code block` and *bold text*.
+
+---
+
+## H2
+
+> Some quote text here
+
+With another line of text here with **italic text**.
+
+Links go here
+
+[link #1](http://localhost/)
+[link #2](https://google.com)
+[link #3](http://starcolon.com)
+or automatic link as http://starcolon.com
+
--- test/api.test.js
+++ test/api.test.js
@@ -1,6 +1,7 @@
 const assert = require('assert')
 
 const Parser = require('../markdown.js');
+const io = require('../lib/io.js');
 
 describe('Parser Interface', () => {
   beforeEach(() => {})
@@ -12,8 +13,22 @@ describe('Parser Interface', () => {
   })
 
   it('should parse text', () => {
+    let txt = `# Headline
+    ## 2nd headline
+    woooh
+    lastline`;
+    
+    let output = `<h1>Headline</h1>
+    <h2>2nd headline woooh
+    lastline`;
 
+    expect(Parser.parseText(txt)).toEqual(output);
   })
 
-  it('should parse file', () => {})
+  it('should parse file', () => {
+    let mdFile = '../samples/primitive.md';
+    let output = io.read('../samples/primitive.html');
+
+    expect(Parser.parseFile(mdFile).toEqual(output));
+  })
 })
