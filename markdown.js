75722783724dbf27c67a5fa095822515c44e5b05
starcolon <tao@starcolon.com>
2018-07-01 21:06:44 +0700

    runnable markdown js

--- markdown.js
+++ markdown.js
@@ -5,5 +5,23 @@
  */
 
 const Parser = require('./lib/parser')
+var colors   = require('colors');
+var args     = process.argv.slice(2);
+
+(function (){
+  // Read arguments
+  if (args.length < 2){
+    console.warn('Expect 2 arguments: markdown.js [inputPath] [outputPath]');
+    return process.exit(0);
+  }
+
+  // Start the parsing job
+  var [inputPath, outputPath] = args;
+  console.log('Input path: '.cyan, inputPath);
+  console.log('outputPath path : '.cyan, outputPath);
+
+  // Finish
+
+})()
 
 module.exports = Parser
