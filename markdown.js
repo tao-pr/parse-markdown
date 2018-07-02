68a15b16660a1e084048acb05fdd2c08f92c174c
starcolon <tao@starcolon.com>
2018-07-03 00:46:05 +0700

    (wip) allow running as standalone and imported module

--- markdown.js
+++ markdown.js
@@ -9,37 +9,39 @@ var colors   = require('colors');
 var fs       = require('fs');
 var args     = process.argv.slice(2);
 
-(function (){
-  // Read arguments
-  if (args.length < 2){
-    console.warn('Expect 2 arguments: markdown.js [inputPath] [outputPath]');
-    return process.exit(0);
-  }
+if (require.main === module) {
+  (function (){
+    // Read arguments
+    if (args.length < 2){
+      console.warn('Expect 2 arguments: markdown.js [inputPath] [outputPath]');
+      return process.exit(0);
+    }
 
-  // Start the parsing job
-  var [inputPath, outputPath] = args;
-  console.log('Input path: '.cyan, inputPath);
-  console.log('outputPath path : '.cyan, outputPath);
+    // Start the parsing job
+    var [inputPath, outputPath] = args;
+    console.log('Input path: '.cyan, inputPath);
+    console.log('outputPath path : '.cyan, outputPath);
 
-  fs.readdir(inputPath, (error,files) => {
-    var batch = [];
-    files.forEach((fpath) => {
-      var inputFile = fpath.toLowerCase()
-      if (inputFile.match(/^(.+)\.md$/)){
-        console.log('... Parsing : ', inputFile);
-        var fullInputPath = inputPath + '/' + inputFile; 
-        var fullOutputPath = outputPath + '/' + inputFile.replace(/\.md$/, '.html');
-        batch.push(Parser.parseFile(fullInputPath).asHTMLFile(fullOutputPath));
-      }
+    fs.readdir(inputPath, (error,files) => {
+      var batch = [];
+      files.forEach((fpath) => {
+        var inputFile = fpath.toLowerCase()
+        if (inputFile.match(/^(.+)\.md$/)){
+          console.log('... Parsing : ', inputFile);
+          var fullInputPath = inputPath + '/' + inputFile; 
+          var fullOutputPath = outputPath + '/' + inputFile.replace(/\.md$/, '.html');
+          batch.push(Parser.parseFile(fullInputPath).asHTMLFile(fullOutputPath));
+        }
+      })
+
+      // Wait until all batch process is complete
+      Promise.all(batch).then(() => {
+        console.log('All files are parsed.'.green);
+        process.exit(0);
+      })
     })
+  })() 
+} else {
+  module.exports = Parser
+}
 
-    // Wait until all batch process is complete
-    Promise.all(batch).then(() => {
-      console.log('All files are parsed.'.green);
-      process.exit(0);
-    })
-  })
-
-})()
-
-module.exports = Parser
