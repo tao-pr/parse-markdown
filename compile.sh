ef02c24402314dd4e6af8dbe0b5866b2dd792713
Tao PR <pataoengineer@gmail.com>
2018-07-07 16:41:16 +0700

    (wip) mkdir output

--- compile.sh
+++ compile.sh
@@ -15,19 +15,37 @@ LRED=$(echo -en '\033[01;31m')
 LGREEN=$(echo -en '\033[01;32m')
 LYELLOW=$(echo -en '\033[01;33m')
 
+if [ -z "$1" ] || [ -z "$2" ];then
+  echo $RED Please specify two arguments: source_dir dest_dir $RESET
+  exit 0
+fi
+
+source_dir="$1"
+dest_dir="$2"
+
 parse_file(){
   local fpath="$1"
-  local source_dir="$2"
-  local dest_root_dir="$3"
-  echo "  f = $fpath"
-  echo "  s = $source_dir"
+  local relpath="$2"
+
+  local n0=${#relpath}
+  local fname=$(basename $fpath)
+
+  local n=${#source_dir}
+  local source_subdir=$(echo "$relpath" | cut -c${n}-)
+
+  local dest_fullpath="${dest_dir}${source_subdir}/${fname}"
+
+  mkdir -p "${dest_dir}${source_subdir}"
+
+  echo "  d = $dest_fullpath"
 }
 
 process_dir(){
   echo Reading files in ${GREEN}$1 ${RESET}
+  local 
   for f in "$1"/*.md
   do
-    parse_file "$f" "$1" "$2"
+    parse_file "$f" "$1"
   done
 
   # Subdir 
@@ -38,8 +56,5 @@ process_dir(){
   done
 }
 
-if [ -z "$1" ] || [ -z "$2" ];then
-  echo $RED Please specify two arguments: source_dir dest_dir $RESET
-  exit 0
-fi
-process_dir "$1" "$2"
+
+process_dir "$source_dir" "$dest_dir"
